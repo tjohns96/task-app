b0f542a8309d7b5de9d25f0fddce5c198baee8e4
tjohns96 <trevorjohnson9601@gmail.com>
2022-09-04 20:21:28 -0400

    Fix redirect url

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -20,7 +20,7 @@ export const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/signedIn",
+  signInSuccessUrl: "/task-app",
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
