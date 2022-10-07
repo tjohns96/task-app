f8de57940890dbb113171e3baaaf2b04f4599f9c
Trevor Johnson <46728195+tjohns96@users.noreply.github.com>
2022-10-07 15:46:59 -0400

    Update firebase-config.js
    Fix the redirect url for netlify deployment

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -20,7 +20,7 @@ export const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/task-app",
+  signInSuccessUrl: "/",
   // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
